dbac8c7afac003be8f3be13b7cfe01f569d9af35
shailesh19 <beingshailesh19@gmail.com>
2019-08-10 16:45:58 +0530

    Implemented Docker Compose and Logger for Muzix App.

--- track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -1,16 +1,28 @@
 package com.stackroute.trackservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 
+
+
+
 @SpringBootApplication
 @EnableEurekaClient
 public class TrackServiceApplication
 {
+	private static Logger logger = (Logger) LoggerFactory.getLogger(TrackServiceApplication.class);
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(TrackServiceApplication.class, args);
+
+		logger.debug("This is Debug Message");
+		logger.info("This is an Info Message");
+		logger.warn("This is a Warn Message");
+		logger.error("This is an error Message");
 	}
 }
--- user-service/src/main/java/com/stackroute/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/stackroute/userservice/UserserviceApplication.java
@@ -1,12 +1,16 @@
 package com.stackroute.userservice;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
